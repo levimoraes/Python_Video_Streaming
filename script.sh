5133f11574a1f8b6a8795373af7a7881da3be3cd
Levi Moraes <levi.moraes@fabricadeideias.com>
2019-06-03 12:23:31 -0300

    Change conf

--- script.sh
+++ script.sh
@@ -10,8 +10,8 @@ cd nginx-1.13.8/
 apt update -y
 apt install autoconf automake build-essential libpcre3 libpcre3-dev libssl-dev -y
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --with-http_stub_status_module --add-module=../nginx-ts-module --without-http_gzip_module
-sudo make
-sudo make install
+make
+make install
 cd /home/odroid/Python_Video_Streaming/
 rm /usr/local/nginx/conf/nginx.conf
 cp nginx.conf /usr/local/nginx/conf/
